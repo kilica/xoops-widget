a840cc1f95d655d091623d099c40ec6ead921bd0
kilica <kilica.hikawa@gmail.com>
2012-10-02 17:48:11 +0900

    Add rss plugin

---
+++ xoops_trust_path/modules/widget/plugins/rss/language/ja_utf8.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ICHIKAWA
+ * Date: 12/07/11
+ * Time: 17:03
+ * To change this template use File | Settings | File Templates.
+ */
+define('_WIDGET_PLUGIN_RSS_TITLE', 'RSS のタイトル');
+define('_WIDGET_PLUGIN_RSS_FEED_URL', 'RSS フィードのURL');
+define('_WIDGET_PLUGIN_RSS_NUMBER', '表示件数');
---
+++ xoops_trust_path/modules/widget/plugins/rss/plugin.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ICHIKAWA
+ * Date: 12/07/11
+ * Time: 19:49
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Widget_Rss_Plugin implements Widget_PluginInterface
+{
+	public static function execute(Widget_InstanceObject $object)
+	{
+		include_once WIDGET_TRUST_PATH.'/vendor/simplepie/autoloader.php';
+		$feed = new SimplePie();
+		$feed->set_feed_url($object->getOptionValue('p_feed_url'));
+		$feed->set_item_limit($object->getOptionValue('p_number'));
+		$upload_path = XOOPS_TRUST_PATH.'/uploads';
+		$widget_path = $upload_path.'/widget';
+		$cache_path = $widget_path.'/rss';
+		if(file_exists($upload_path)===true && is_writable($upload_path)===true && file_exists($cache_path)===false){
+			if(file_exists($widget_path)===false){
+				mkdir($upload_path.'/widget');
+				chmod($upload_path.'/widget', 0777);
+			}
+			mkdir($cache_path);
+			chmod($cache_path, 0777);
+		}
+		if(file_exists($cache_path)===true && is_writable($cache_path)===true){
+			$feed->set_cache_location($cache_path);
+		}
+		else{
+			$feed->enable_cache(false);
+		}
+		$feed->init();
+		$feed->handle_content_type();
+		$object->mFeed = $feed;
+	}
+
+	public static function prepareEditform(Widget_InstanceObject $object)
+	{
+	}
+
+	public static function fetch(Widget_InstanceObject $object, $request)
+	{
+
+	}
+
+	public static function getImageNumber(Widget_InstanceObject $obj)
+	{
+		return 0;
+	}
+
+}
